b131b75560e4aac3a44726e12539d0620b03dae1
Fernando <f.miranda25d@gmail.com>
2026-06-04 13:16:46 -0500

    Create and configure data source

--- src/main/java/com/pluralsight/Program.java
+++ src/main/java/com/pluralsight/Program.java
@@ -1,4 +1,13 @@
 package com.pluralsight;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+
 public class Program {
+    public static void main(String[] args) {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setUrl("jdbc:mysql://localhost:3306/sakila");
+        dataSource.setUsername("root");
+        dataSource.setPassword("");
+
+    }
 }
